dd1e67c408860cf04aed20a56157ca208f4aa040
Dong.H Kim <hinain@gmail.com>
2010-07-23 17:19:31 +0900

    first commit

---
+++ target/mkdirs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir dev
+mknod dev/console c 5 1
+mkdir dev/pts
+mkdir etc
+mkdir etc/rc.d
+mkdir lib
+mkdir mnt
+mkdir opt
+mkdir proc
+mkdir root
+mkdir sys
+mkdir tmp
+mkdir var
+mkdir var/log
